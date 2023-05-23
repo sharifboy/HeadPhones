dce005c576ca82dbcc33664ab8302718518270e4
sharifboy <sharifboy@gmail.com>
2023-05-23 13:40:27 +0500

    added another code

--- src/components/Header/HeaderNav/index.jsx
+++ src/components/Header/HeaderNav/index.jsx
@@ -1,10 +1,21 @@
 import React from "react";
+import logo from "../../../assets/image/logo.png"
+import search from "../../../assets/image/search.png"
 
 const HeaderNav = () => {
   return (
     <div className="header-nav">
       <div className="container">
-        <h1>HeaderNav</h1>
+        <div className="content">
+          <a href="/" className="logo">
+            <img src={logo} alt="logo" />
+          </a>
+          <nav className="header-nav__navv">
+            <div className="header-nav__nav-link">
+              <img src={search} alt="search" />
+            </div>
+          </nav>
+        </div>
       </div>
     </div>
   );
