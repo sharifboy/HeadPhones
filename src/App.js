884d96ff0360e53e6bc6cc749661483c1d71fb7c
sharifboy <sharifboy@gmail.com>
2023-05-30 15:24:23 +0500

    added another code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,10 @@
 import React from "react";
 import Header from "./components/Header";
-import Form from "./components/Form";
 
 function App() {
-  
   return (
     <>
       <Header />
-      <Form />
     </>
   );
 }
--- src/components/Header/HeaderInfo/index.jsx
+++ src/components/Header/HeaderInfo/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 
-import "./style.css"
+import "./style.css";
 import hero from "../../../assets/image/headphone.png";
 
 const HeaderInfo = () => {
@@ -15,8 +15,8 @@ const HeaderInfo = () => {
             <span className="subtitle">hear it. feel it</span>
             <h1 className="main-title">move with the music</h1>
             <div className="price-box">
-                <span className="discount-price">$ 435</span>
-                <span className="old-price">$ 465</span>
+              <span className="discount-price">$ 435</span>
+              <span className="old-price">$ 465</span>
             </div>
             <button className="button btn-light">buy now</button>
           </div>
